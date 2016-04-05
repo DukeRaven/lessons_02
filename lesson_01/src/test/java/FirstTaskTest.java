b1989b97f6f55c679aa7560772474da81fe53289
Kirill Khokhlachev <khokhlachev@bpcbt.com>
2016-04-05 13:32:28 +0300

    Import Assert.* Boolean assertion

--- lesson_01/src/test/java/FirstTaskTest.java
+++ lesson_01/src/test/java/FirstTaskTest.java
@@ -1,11 +1,12 @@
 import com.kirill.firsttask.GenericStack;
 import com.kirill.firsttask.StackException;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by Kirill on 04.04.2016.
@@ -17,31 +18,34 @@ public class FirstTaskTest {
         GenericStack<Integer> stack = new GenericStack<>();
         Integer value = new Integer(1);
 
-        Assert.assertEquals(true, stack.isEmpty());
-        Assert.assertEquals(false, stack.isFull());
-        Assert.assertEquals(0, stack.getSize());
+        assertTrue(stack.isEmpty());
+        assertFalse(stack.isFull());
+        assertEquals(0, stack.getSize());
 
         stack.push(new Integer(1));
-        Assert.assertEquals(1, stack.getSize());
-        Assert.assertEquals(value, stack.peek());
-        Assert.assertEquals(value, stack.pop());
+        assertEquals(1, stack.getSize());
+        assertEquals(value, stack.peek());
+        assertEquals(value, stack.pop());
 
-        Assert.assertEquals(true, stack.isEmpty());
-        Assert.assertEquals(false, stack.isFull());
+        assertTrue(stack.isEmpty());
+        assertFalse(stack.isFull());
 
         List<Integer> srcList = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
             srcList.add(new Integer(i));
         }
         stack.pushAll(srcList);
-        Assert.assertEquals(10, stack.getSize());
+        assertEquals(10, stack.getSize());
 
         List<Integer> dstList = new ArrayList<>();
         stack.popAll(dstList);
 
         Collections.sort(dstList);
-        Assert.assertArrayEquals(srcList.toArray(), dstList.toArray());
+        assertArrayEquals(srcList.toArray(), dstList.toArray());
     }
+
+
+
 
     @Test(expected = StackException.class)
     public void testPushException() throws StackException {
